0e2f009327014e9581dbf5627aef48fbe6a5ecdb
Damian Mrozowski <68596121+gastronleroux@users.noreply.github.com>
2022-12-01 19:12:22 +0100

    Create part-2.rb

---
+++ day-1/part-2.rb
@@ -0,0 +1,27 @@
+class CaloriesHunter
+  def initialize(input)
+    @input = input
+  end
+
+  def hunt
+    sum_of_top_3_calories
+  end
+
+  private
+
+  attr_reader :input
+
+  def sum_of_top_3_calories
+    summed_calories.max(3).sum
+  end
+
+  def summed_calories
+    grouped_calories.map(&:sum)
+  end
+
+  def grouped_calories
+    input.split(/\n\n/).map do |string_calories|
+      string_calories.split(/\n/).map(&:to_i)
+    end
+  end
+end
